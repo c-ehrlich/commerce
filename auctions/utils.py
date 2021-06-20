06539128d39bae431b3932a67a4e38d7eeb58288
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-06-20 13:51:30 +0200

    add category functionality to add_auctions

--- auctions/utils.py
+++ auctions/utils.py
@@ -7,28 +7,28 @@ from .models import Auction, Bid, User
 # input: cleaned_data of the create auction form
 # output: if successful -> auction id | if unsuccessful -> -1
 def create_auction(request, data):
-    print(f"data: {data}")
-    print(type(data))
-    auction = Auction(
-		seller = request.user,
-		title = data["title"],
-		description = data["description"],
-		# category = data["category"],
-		# category.set(data["category"]),
-		image = data["image"], 
-		starting_bid = data["starting_bid"],
-		ending_time = datetime.now() + dt.timedelta(days = int(data["duration"]))
-	)
-    # auction.category.set(data["category"])
-    auction.save()
-    print(auction.id)
+    try:
+        auction = Auction.objects.create(
+            seller = request.user,
+            title = data["title"],
+            description = data["description"],
+            image = data["image"], 
+            starting_bid = data["starting_bid"],
+            ending_time = datetime.now() + dt.timedelta(days = int(data["duration"]))
+        )
+        auction.category.set({data["category"]})
+        return auction.id
+    except Exception as e:
+        print(e)
+        return -1
+
 
 # returns a dictionary with additional information about an auction
 def get_auction_status(request, auction_id):
     auction = Auction.objects.get(pk=auction_id)
     user = User.objects.get(username=request.user.username)
-	# TODO MAKE SURE I'M NOT DOING INCORRECT TIMEZONE MATH HERE!
-	# (using datetime in UTC, make sure the model also uses UTC)
+    # TODO MAKE SURE I'M NOT DOING INCORRECT TIMEZONE MATH HERE!
+    # (using datetime in UTC, make sure the model also uses UTC)
     if auction.ending_time < datetime.now().astimezone(tz=None):
         is_over = True
     else:
@@ -46,16 +46,16 @@ def get_auction_status(request, auction_id):
 
 # returns an object representing the current bid
 def get_current_bid(auction_id):
-	auction = Auction.objects.get(pk=auction_id)
-	current_bid = {
-		"price": auction.starting_bid,
-		"bidder": None
-	}
-	bids = auction.auction_bids.all()
-	for bid in bids:
-		if bid.value > current_bid["price"]:
-			current_bid["price"] = bid.value
-			current_bid["bidder"] = bid.user.username
-	print(f"current_bid for {auction_id}: {current_bid}")
-	return current_bid
-	
+    auction = Auction.objects.get(pk=auction_id)
+    current_bid = {
+        "price": auction.starting_bid,
+        "bidder": None
+    }
+    bids = auction.auction_bids.all()
+    for bid in bids:
+        if bid.value > current_bid["price"]:
+            current_bid["price"] = bid.value
+            current_bid["bidder"] = bid.user.username
+    print(f"current_bid for {auction_id}: {current_bid}")
+    return current_bid
+    
